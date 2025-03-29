d29c674cce65a2c61b95630ff68606ecad031ebc
DiegoFlorez579 <diego.florez579@gmail.com>
2025-03-29 17:52:12 -0500

    Cambio de inicio en main

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL").replace("mysql://", "mysql+pymysql://")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
