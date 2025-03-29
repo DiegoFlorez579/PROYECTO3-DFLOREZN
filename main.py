827d81a497a7da878fd289f30918d03860b45cbc
DiegoFlorez579 <diego.florez579@gmail.com>
2025-03-29 17:01:24 -0500

    Cambio de puerto en app

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    #port = int(os.environ.get("PORT", 8080))
+    #app.run(host="0.0.0.0", port=port)
+    pass
 
