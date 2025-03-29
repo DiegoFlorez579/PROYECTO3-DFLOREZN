ff2699ab99e61bf77664234630442fd8ed5d9292
DiegoFlorez579 <diego.florez579@gmail.com>
2025-03-29 17:26:47 -0500

    Cambio de inicio en main

--- config/config.py
+++ config/config.py
@@ -1,7 +1,12 @@
 import os
 
+#class Config:
+#    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL").replace("mysql://", "mysql+pymysql://")
+#    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 DATABASE_URL = os.getenv("DATABASE_URL", "")
 if DATABASE_URL:
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
+    DATABASE_URL = DATABASE_URL.replace("mysql://", "mysql+pymysql://")
 else:
     print("❌ ERROR: No se encontró DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
